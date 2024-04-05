f918780aaf1300e0bdcfc2af68260f4b05122680
invi1998 <2267661826@qq.com>
2024-04-05 15:27:35 +0800

    AuraPlayerController

---
+++ Source/Arcane/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright INVI1998
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/Arcane/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright INVI1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANE_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
