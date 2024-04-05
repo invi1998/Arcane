9e5269371f43163087588ae56aa62ae99c54457f
invi1998 <2267661826@qq.com>
2024-04-06 00:46:23 +0800

    设置PlayerState的Effect复制模式Mixed

--- Source/Arcane/Private/Player/AuraPlayerState.cpp
+++ Source/Arcane/Private/Player/AuraPlayerState.cpp
@@ -12,6 +12,7 @@ AAuraPlayerState::AAuraPlayerState()
 
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>(TEXT("AbilitySystemComponent"));	// 创建能力系统组件
 	AbilitySystemComponent->SetIsReplicated(true);	// 设置为可复制
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);	// 设置复制模式，混合模式，对于多人游戏来说，特别是玩家控制的角色，设置为Mixed是最好的选择
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>(TEXT("AttributeSet"));		// 创建属性集
 
