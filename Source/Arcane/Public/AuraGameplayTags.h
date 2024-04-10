4abd4de5f052a0b175a73ad1b00bd5fdf489522f
invi1998 <2267661826@qq.com>
2024-04-10 17:24:42 +0800

    将所有primay和Secondary属性都添加到我们自己的GamplayTag中，并通过我们自己管理器进行管理

--- Source/Arcane/Public/AuraGameplayTags.h
+++ Source/Arcane/Public/AuraGameplayTags.h
@@ -15,9 +15,33 @@ struct FAuraGameplayTags
 public:
 	static const FAuraGameplayTags& Get() { return GameplayTags; };
 
-	static void InitializeNativeGameplayTags();		// 初始化原生GameplayTags
+	static void InitializeNativeGameplayTags();				// 初始化原生GameplayTags
 
-	FGameplayTag Attributes_Secondary_Armor;			// 二级属性：护甲
+	/*
+	 * 主属性（Primary Attributes）
+	 */
+	FGameplayTag Attributes_Primary_Strength;				// 主属性：力量
+	FGameplayTag Attributes_Primary_Agility;				// 主属性：敏捷
+	FGameplayTag Attributes_Primary_Intelligence;			// 主属性：智力
+	FGameplayTag Attributes_Primary_Vigor;					// 主属性：活力
+	FGameplayTag Attributes_Primary_Resilience;				// 主属性：坚韧
+
+	/*
+	 *	次要属性（Secondary Attributes） 或者说是派生属性（Derived Attributes）
+	 */
+
+	FGameplayTag Attributes_Secondary_MaxHealth;			// 二级属性：最大生命值
+	FGameplayTag Attributes_Secondary_MaxMana;				// 二级属性：最大法力值
+
+	FGameplayTag Attributes_Secondary_Armor;				// 二级属性：护甲
+	FGameplayTag Attributes_Secondary_ArmorPenetration;		// 二级属性：护甲穿透
+	FGameplayTag Attributes_Secondary_BlockChance;			// 二级属性：格挡几率
+	FGameplayTag Attributes_Secondary_CriticalHitChance;	// 二级属性：暴击几率
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;	// 二级属性: 暴击伤害
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;	// 二级属性: 暴击抗性
+	FGameplayTag Attributes_Secondary_HealthRegeneration;	// 二级属性: 生命回复
+	FGameplayTag Attributes_Secondary_ManaRegeneration;		// 二级属性: 法力回复
+
 
 protected:
 
