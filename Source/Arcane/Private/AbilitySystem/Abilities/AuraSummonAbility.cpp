0358f210ac010a55a4c7ff51b9cca5e879c9f4ce
invi1998 <2267661826@qq.com>
2024-04-24 11:56:41 +0800

    召唤技能添加，同时修改Lich施法的目标选取逻辑，在施法的时候重新取一次目标位置，判定更严谨

---
+++ Source/Arcane/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,23 @@
+// Copyright INVI1998
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);	// 旋转SpawnSpread角度/2, 得到左边的向量
+	const FVector RightOfSpread = Forward.RotateAngleAxis(SpawnSpread / 2.f, FVector::UpVector);	// 旋转-SpawnSpread角度/2, 得到右边的向量
+
+	// debug DrawDebugArrow
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + LeftOfSpread * MaxSpawnDistance, 4.f, FColor::Purple, 10.f);
+
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + RightOfSpread * MaxSpawnDistance, 4.f, FColor::Orange, 10.f);
+
+
+	return TArray<FVector>();
+}
---
+++ Source/Arcane/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,36 @@
+// Copyright INVI1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 召唤类技能
+ */
+UCLASS()
+class ARCANE_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Summoning")
+	TArray<FVector> GetSpawnLocations();	// 获取召唤的位置
+
+	UPROPERTY(EditAnywhere, Category = "Summoning")
+	int32 NumMinions = 0;	// 召唤的数量
+
+	UPROPERTY(EditAnywhere, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;	// 召唤的类
+
+	UPROPERTY(EditAnywhere, Category = "Summoning")
+	float MinSpawnDistance = 100.f;		// 最小召唤距离
+
+	UPROPERTY(EditAnywhere, Category = "Summoning")
+	float MaxSpawnDistance = 500.0f;	// 最大召唤距离
+
+	UPROPERTY(EditAnywhere, Category = "Summoning")
+	float SpawnSpread = 100.0f;	// 召唤的散布(分布), 该值越大, 召唤物体的位置范围越大
+};
