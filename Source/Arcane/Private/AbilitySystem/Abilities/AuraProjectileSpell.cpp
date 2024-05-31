86257e2108189e6e1c3cfe343ea74870c0480d52
invi1998 <2267661826@qq.com>
2024-05-31 11:17:04 +0800

    下一级技能的描述信息

--- Source/Arcane/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Arcane/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -41,8 +41,31 @@ FString UAuraProjectileSpell::GetDescription(int32 Level)
 
 FString UAuraProjectileSpell::GetNextLevelDescription(int32 Level)
 {
-	const int32 Damage = DamageType[FAuraGameplayTags::Get().Damage_Fire].GetValueAtLevel(Level);
-	return FString::Printf(TEXT("<Title>NEXT LEVEL: </>\n\n<Default>Launches %d bolts of fire, exploding on impact and dealing: </><Damage>%d</><Default> fire damage with a chance to burn</>\n\n<Small>Level: </><Level>%d</>"), FMath::Min(Level, NumProjectiles), Damage, Level);
+	// 更具Tag获取法力消耗
+	const float Cooldown = CooldownScalableFloat.GetValueAtLevel(Level + 1);
+	const float ManaCost = ManaCostScalableFloat.GetValueAtLevel(Level + 1);
+
+	// 遍历伤害类型，获取每种类型的伤害值，然后拼接字符串
+	FString DamageTypeString;
+	for (auto& DamagePair : DamageType)
+	{
+		const FGameplayTag& DamageTag = DamagePair.Key;
+		const FScalableFloat& DamageValue = DamagePair.Value;
+		const float ScaledDamageValue = DamageValue.GetValueAtLevel(Level + 1);
+		DamageTypeString += FString::Printf(TEXT("\t<Default>%s：</><Damage>%.2f</>\n"), *DamageTag.GetTagName().ToString(), ScaledDamageValue);
+	}
+
+	FString Desc = FString::Printf(TEXT("<Title>火球术</>\t<Small>FIRE BOLT</>\n\n"
+		"<Default>发射一道火焰，在撞击和造成伤害时爆炸 </>\n\n"
+		"\t<Default>技能等级：</><Level>%d</>\n"
+		"\t<Default>冷却时间：</><Cooldown>%.1f s</>\n"
+		"\t<Default>法力消耗：</><ManaCast>%.1f</>\n"
+		"\t<Default>火球数量：</><Time>%d</>\n\n"
+		"<Default>技能详细伤害描述：</>\n"
+	), Level + 1, Cooldown, ManaCost, NumProjectiles);
+	Desc += DamageTypeString;
+
+	return Desc;
 }
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
