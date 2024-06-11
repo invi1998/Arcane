fde97d0f0d3f2ad588d420f49a920154ecd54d4f
invi1998 <2267661826@qq.com>
2024-06-11 14:18:11 +0800

    多弹道算法

--- Source/Arcane/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Arcane/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -4,6 +4,8 @@
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
 
 #include "AuraGameplayTags.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
@@ -63,4 +65,61 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 	Desc += DamageTypeString;
 
 	return Desc;
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,AActor* HomingTarget, bool bOverridePitch, float Pitch)
+{
+	// 投射物生成，我们希望他是在服务端生成，然后在客户端同步
+	const bool bIsServer = GetOwningActorFromActorInfo()->HasAuthority();
+	if (!bIsServer) return;
+
+	// 生成位置，我不希望简单使用角色的位置，而是使用施法者武器上的插槽位置
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+
+	if (CombatInterface)
+	{
+		const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
+		FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();	// 获取朝向目标的旋转
+		// 此时，如果怪物身高高于或者低于角色，那么投射物的朝向可能会有问题，我们需要调整一下
+		// 因为我们希望投射物能尽可能平行，所以这里把Pith调整为0
+		// Rotation.Pitch = 0.f;
+
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);		// 使用武器插槽位置
+		SpawnTransform.SetRotation(Rotation.Quaternion());	// 设置旋转(这里需要传入四元数）
+
+		if (bOverridePitch)
+		{
+			SpawnTransform.SetRotation(FRotator(Pitch, Rotation.Yaw, Rotation.Roll).Quaternion());
+		}
+
+		const FVector ForwardVector = Rotation.Vector();
+		const FVector LeftVector = ForwardVector.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+
+		// const int32 RealNumProjectiles = FMath::Min(MaxProjectileCount, GetAbilityLevel());
+
+		const int32 RealNumProjectiles = 10;
+
+		const float DeltaSpread = ProjectileSpread / FMath::Max(1, RealNumProjectiles - 1);
+
+		
+		if (RealNumProjectiles > 1)
+		{
+			for (int32 i = 0; i < RealNumProjectiles; ++i)
+			{
+				const FVector SpreadVector = LeftVector.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+				const FRotator SpreadRotation = SpreadVector.Rotation();
+
+				// 在生成点和目标点之间生成贝塞尔曲线
+
+				UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + SpreadVector * 1000.f, 100.f, FLinearColor::Green, 125.f, 1.f);
+			}
+		}
+		else
+		{
+			
+		}
+
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Rotation.Vector() * 1000.f, 100.f, FLinearColor::Red, 125.f, 5.f);
+	}
 }
--- Source/Arcane/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Arcane/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -17,5 +17,15 @@ class ARCANE_API UAuraFireBolt : public UAuraProjectileSpell
 public:
 	virtual FString GetDescription(int32 Level) override;
 	virtual FString GetNextLevelDescription(int32 Level) override;
-	
+
+	UFUNCTION(BlueprintCallable, Category = "Aura Ability")
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, AActor* HomingTarget, bool bOverridePitch = false, float Pitch = 0.0f);
+
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Aura Ability")
+	float ProjectileSpread = 90.0f;		// 投射物散射角度
+
+	UPROPERTY(EditDefaultsOnly, Category = "Aura Ability")
+	int32 MaxProjectileCount = 10;		// 最大投射物数量
 };
