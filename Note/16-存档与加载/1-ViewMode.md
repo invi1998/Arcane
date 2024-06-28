a35b2925e1af36f7e47c99c16ca486f2f1ee4967
invi1998 <2267661826@qq.com>
2024-06-28 16:14:15 +0800

    mvvm

---
+++ Note/16-存档与加载/1-ViewMode.md
@@ -0,0 +1,9 @@
+如果要在系统中使用ModelViewViewMode（MVVM），首先需要启用插件
+
+![image-20240628160644931](.\image-20240628160644931.png)
+
+
+
+然后基于MVVMViewModelBase创建C++类
+
+![image-20240628161102911](.\image-20240628161102911.png)
---
+++ Source/Arcane/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -0,0 +1,17 @@
+// Copyright INVI1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "MVVM_LoadScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANE_API UMVVM_LoadScreen : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+	
+};
