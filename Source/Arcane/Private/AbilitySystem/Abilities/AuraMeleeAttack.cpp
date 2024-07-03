fe87a9a60e207091a6b03c2806311937f02cbafc
invi1998 <2267661826@qq.com>
2024-07-03 23:50:26 +0800

    武僧连招伤害实现

--- Source/Arcane/Private/AbilitySystem/Abilities/AuraMeleeAttack.cpp
+++ Source/Arcane/Private/AbilitySystem/Abilities/AuraMeleeAttack.cpp
@@ -3,3 +3,54 @@
 
 #include "AbilitySystem/Abilities/AuraMeleeAttack.h"
 
+#include "Character/AuraCharacter.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraMeleeAttack::GenerateAttackTraceSockets(const FName& SocketName, bool bWeapon, float ForwardOffset, int32 Num, float TotalLength)
+{
+	TArray<FVector> TraceSockets;
+
+	if (CurrentActorInfo)
+	{
+		const AAuraCharacter* OwnerCharacter = Cast<AAuraCharacter>(CurrentActorInfo->AvatarActor.Get());
+
+		if (OwnerCharacter && OwnerCharacter->Implements<UCombatInterface>())
+		{
+			const USkeletalMeshComponent* Mesh = bWeapon ? ICombatInterface::Execute_GetWeaponMesh(OwnerCharacter) : OwnerCharacter->GetMesh();
+
+			if (Mesh)
+			{
+				const FVector SocketLocation = Mesh->GetSocketLocation(SocketName);
+
+				// 获取插槽的前向向量（及得归一化）
+				const FVector ForwardVector = Mesh->GetSocketRotation(SocketName).Vector().GetSafeNormal();
+
+				//// 绘制插槽位置
+				//UKismetSystemLibrary::DrawDebugSphere(GetWorld(), SocketLocation, 10.f, 12, FLinearColor::Red, 5.f);
+				//// 绘制插槽前向向量
+				//UKismetSystemLibrary::DrawDebugArrow(GetWorld(), SocketLocation, SocketLocation + ForwardVector * 100.f, 100.f, FLinearColor::Green, 5.f);
+
+				// 基于偏移量计算新的插值起始位置
+				const FVector StartLocation = SocketLocation + (ForwardVector * ForwardOffset);
+
+				if (Num == 1)
+				{
+					TraceSockets.Add(StartLocation);
+				}
+				else if (Num > 1)
+				{
+					// 生成一组插槽位置
+					const float offset = TotalLength / (Num - 1);
+					for (int32 i = 0; i < Num; i++)
+					{
+						TraceSockets.Add(StartLocation + (ForwardVector * offset * i));
+					}
+				}
+				
+			}
+		}
+	}
+
+	return TraceSockets;
+}
--- Source/Arcane/Public/AbilitySystem/Abilities/AuraMeleeAttack.h
+++ Source/Arcane/Public/AbilitySystem/Abilities/AuraMeleeAttack.h
@@ -13,5 +13,10 @@ UCLASS()
 class ARCANE_API UAuraMeleeAttack : public UAuraDamageGameplayAbility
 {
 	GENERATED_BODY()
+
+public:
+	// 传入Socket名称，传入是否是武器插槽或者是角色骨骼插槽，传入socket位置偏移量（默认为0），传入检测点数量（默认为1），传入检测长度（默认为100），生成一组检测点用于攻击检测
+	UFUNCTION(BlueprintCallable, Category = "Melee Attack")
+	TArray<FVector> GenerateAttackTraceSockets(const FName& SocketName, bool bWeapon, float ForwardOffset = 0.f, int32 Num = 1, float TotalLength = 100.0f);
 	
 };
