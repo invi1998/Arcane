41c89303f83e04b649f571432912e09e67dedfbb
invi1998 <2267661826@qq.com>
2024-07-23 16:42:37 +0800

    检查点

--- Source/Arcane/Private/Checkpoint/Checkpoint.cpp
+++ Source/Arcane/Private/Checkpoint/Checkpoint.cpp
@@ -12,17 +12,18 @@ ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer) : Super(Ob
 
 	PrimaryActorTick.bCanEverTick = false;
 
-	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
-	CollisionSphere->SetupAttachment(GetRootComponent());
-	CollisionSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	CollisionSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
-	CollisionSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-
 	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CheckpointMesh"));
 	CheckpointMesh->SetupAttachment(GetRootComponent());
 	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
 	CheckpointMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+	CollisionSphere->SetupAttachment(CheckpointMesh);
+	CollisionSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CollisionSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
 }
 
 void ACheckpoint::BeginPlay()
@@ -35,7 +36,22 @@ void ACheckpoint::BeginPlay()
 
 void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	
+	// 只有玩家角色进入的时候才会触发
+	if (OtherActor->ActorHasTag("Player"))
+	{
+		HandleGlowEffects();
+	}
+}
+
+void ACheckpoint::HandleGlowEffects()
+{
+	CollisionSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// 创建一个动态材质实例，基于当前的材质
+	UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	CheckpointMesh->SetMaterial(0, DynamicMaterial);
+
+	CheckpointReached(DynamicMaterial);
 }
 
 
--- Source/Arcane/Public/Checkpoint/Checkpoint.h
+++ Source/Arcane/Public/Checkpoint/Checkpoint.h
@@ -23,6 +23,10 @@ protected:
 	UFUNCTION()
 	virtual void OnSphereOverlap(UPrimitiveComponent*OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);	// 碰撞体重叠事件
 
+	UFUNCTION(BlueprintImplementableEvent)	// 蓝图实现的事件
+	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialIns);	// 检查点到达
+
+	void HandleGlowEffects();	// 处理光效
 	
 
 private:
@@ -31,5 +35,7 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UShapeComponent> CollisionSphere;	// 碰撞体
+
+
 	
 };
