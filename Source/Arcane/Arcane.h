97584eac7ff49aa185fd98a073178474f8ff54b5
invi1998 <2267661826@qq.com>
2024-07-26 17:06:16 +0800

    新增两个自定义深度值

--- Source/Arcane/Arcane.h
+++ Source/Arcane/Arcane.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 
-#define CUSTOM_DEPTH_STENCIL_RED 250	// 红色自定义深度缓冲值
+#define CUSTOM_DEPTH_STENCIL_RED 250		// 红色自定义深度缓冲值
+#define CUSTOM_DEPTH_STENCIL_BLUE 251		// 蓝色自定义深度缓冲值
+#define CUSTOM_DEPTH_STENCIL_TAN 252		// 米黄色自定义深度缓冲值
 
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1	// 子弹碰撞频道
 #define ECC_Target ECollisionChannel::ECC_GameTraceChannel2		// 目标碰撞频道
